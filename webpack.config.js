579117d5a83c30d846cadaed44f7fb95224935af
Anil Kumar K K <akumarkk@outlook.com>
2023-06-12 21:55:02 -0700

    babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,18 @@ module.exports = {
   devServer: {
     static: "./dist",
   },
+  module: {
+    rules: [
+      {
+        test : /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+              presets: ["@babel/preset-env"],
+          }
+       },
+      }
+    ]
+  }
 };
